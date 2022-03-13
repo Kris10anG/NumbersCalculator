40561d511d8abc53a2ba899dbfcb8fc5695f46ad
Kris10anG <kristiang@getacademy.no>
2022-03-13 21:23:38 +0100

    Add project files.

---
+++ Kodetime 28.02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kodetime_28._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 4};
+            var numberprossesor = new numberProssesor();
+            var mostFrequentNumber = numberprossesor.MostFrequentNumber(numbers);
+            Console.WriteLine("Mest gjentatte nummer " + mostFrequentNumber);
+
+            var BiggestNumber = numberprossesor.BiggestNumber(numbers);
+            Console.WriteLine("Høyeste nummer " + BiggestNumber);
+
+            var LowestNumber = numberprossesor.LowestNumber(numbers);
+            Console.WriteLine("Laveste nummer " + LowestNumber);
+
+            var summary = numberprossesor.Summary(numbers);
+            Console.WriteLine("Summen av listen er " + summary);
+
+
+        }
+
+    }
+}
---
+++ Kodetime 28.02/numberProssesor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodetime_28._02
+{
+    internal class numberProssesor
+    {
+        public int MostFrequentNumber(int[] numbers)
+        {
+            var count = 0;
+            var mostnumbers = 0;
+            var number = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                        if (count > mostnumbers)
+                        {
+                            mostnumbers = count;
+                            number = numbers[i];
+                        }
+                    }
+                }
+                count = 0;
+
+            }
+
+            return number;
+            //Console.WriteLine(number);
+        }
+
+        public int BiggestNumber(int[] numbers)
+        {
+            var biggestnumber = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (biggestnumber <= numbers[i])
+                {
+                    biggestnumber = numbers[i];
+                }
+            }
+
+            return biggestnumber;
+        }
+
+        public int LowestNumber(int[] numbers)
+        {
+            var lowestnumber = numbers[0];
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if (lowestnumber >= numbers[i])
+                {
+                    lowestnumber = numbers[i];
+
+                }
+                
+            }
+            return lowestnumber;
+        }
+
+        public int Summary(int[] numbers)
+        {
+            var sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+              sum += numbers[i];
+            }
+            return sum;
+        }
+}
+}
